5d1040391f83c26d09a1120aa4473314bf2a033e
Brant <brantrusnakx95@gmail.com>
2017-10-03 14:25:47 -0400

    Adding main files

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FeatureModule } from 'angular-feature-module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FeatureModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
